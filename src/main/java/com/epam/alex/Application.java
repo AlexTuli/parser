2a046ba429c0cd464d644093964fed236aa8d640
Alexander Bocharnikov <balexander1995@gmail.com>
2016-02-12 17:04:51 +0600

    Add file upload function

---
+++ src/main/java/com/epam/alex/Application.java
@@ -0,0 +1,17 @@
+package com.epam.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2/12/2016.
+ *
+ * @author Bocharnikov Alexander
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
