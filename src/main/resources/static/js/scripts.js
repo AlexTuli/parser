c7cf204c2e7b5c80af3ad7041bb500fa9b98f285
Alexander Bocharnikov <balexander1995@gmail.com>
2016-02-17 10:35:04 +0600

    Finish with index.html, add Angular

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,5 @@
+function chooseParser(actionName) {
+            document.getElementById("parser-form").setAttribute('action','/' + actionName);
+            document.getElementById("parser-name").innerHTML = actionName.toUpperCase() + " parser";
+            $("#parser").collapse("show");
+        };
