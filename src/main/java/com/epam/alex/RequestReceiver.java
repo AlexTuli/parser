ab45e5e837d3258a067d252556405914dfadae3c
Alexander Bocharnikov <balexander1995@gmail.com>
2016-02-18 17:39:41 +0600

    ...

--- src/main/java/com/epam/alex/RequestReceiver.java
+++ src/main/java/com/epam/alex/RequestReceiver.java
@@ -3,28 +3,20 @@ package com.epam.alex;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created on 2/17/2016.
  *
  * @author Bocharnikov Alexander
  */
-@Controller
+@RestController
 public class RequestReceiver {
 
     @RequestMapping("/sax")
-    public @ResponseBody void sayHello(){
+    public @ResponseBody HelloWorld sayHello(){
         System.out.println("\n\nSax parser is was called!\n");
-    }
-
-    @RequestMapping("/dom")
-    public void saySay () {
-        System.out.println("\n\nDOM parser is was called.\n");
-    }
-
-    @RequestMapping("/stax")
-    public void saySayIt () {
-        System.out.println("\n\nDOM parser is was called.\n");
+        return new HelloWorld("Hello, world!");
     }
 
 }
--- src/main/resources/static/js/angularApp.js
+++ src/main/resources/static/js/angularApp.js
@@ -2,12 +2,34 @@
 var app = angular.module('parserApp', []);
 
 app.controller('bodyController', function() {
-   this.current = 1;
+    this.current = 1;
    this.selectBody = function (newValue){
       this.current = newValue;
-   }
+   };
    this.isSelected = function (value) {
       return this.current === value;
    }
 });
+
+    app.controller('saxParserRequestController', [ '$http', function($http){
+        var vm = this;
+        vm.answer = [];
+        $http({
+            method: "GET",
+            url: "/sax"
+        }).then (function successCallback (responce) {
+            vm.answer = responce.data;
+    }, function errorCallback (responce) {
+        vm.answer = responce.statusText;
+    });
+
+    }]);
+
+    app.controller('parserController', function(){
+        this.typeOfParser = "sax";
+        this.setTypeOfParser = function(newType){
+            this.typeOfParser = newType;
+        }
+    });
+
 })();
